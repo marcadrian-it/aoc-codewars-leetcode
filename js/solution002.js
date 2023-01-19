4752b9e837d06947ad7074e9ee12d95013741098
marcadrian <marcadrian.it@gmail.com>
2023-01-19 11:42:39 +0100

    Update solution002.js

--- js/solution002.js
+++ js/solution002.js
@@ -1,3 +1,4 @@
+//There is an array with some numbers. All numbers are equal except for one. Try to find it!
 function findUniq(arr) {
     arr.sort((a,b)=>a-b);
     return arr[0]==arr[1]?arr.pop():arr[0]
