27e34863b2a95010ed969dea8d208632d7f720f3
marcadrian-it <marcadrian.it@gmail.com>
2023-01-23 16:25:42 +0100

    solution006

---
+++ js/solution006.js
@@ -0,0 +1,9 @@
+// fibonacci but with last 3  values
+function tribonacci(signature,n){
+    if(n==0) return [];
+    for (var i = 0; i < n-3; i++) { // iterate n times
+      signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
+    }
+    return signature.slice(0, n); //return trib - length of n
+  }
+  
