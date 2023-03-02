4f719374310f65a23413f73dcb3f376e821c6d34
marcadrian-it <marcadrian.it@gmail.com>
2023-03-02 19:28:48 +0100

    solution043.js

---
+++ js/solution043.js
@@ -0,0 +1,10 @@
+function whatCentury(year) {
+    const century = Math.ceil(year / 100);
+    if (century > 10 && century < 20) return century + "th";
+    switch (century % 10) {
+      case 1: return century + "st";
+      case 2: return century + "nd";
+      case 3: return century + "rd";
+      default: return century + "th";
+    }
+  }
