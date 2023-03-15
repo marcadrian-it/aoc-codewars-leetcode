ddb17b9047d43e8deb0811fbb1ac88efb925744d
marcadrian-it <marcadrian.it@gmail.com>
2023-03-15 21:28:47 +0100

    solution056.js

---
+++ js/solution056.js
@@ -0,0 +1,23 @@
+function alphabetWar(fight) {
+  let scoreRight = 0;
+  let scoreLeft = 0;
+  const left = ["s", "b", "p", "w"];
+  const right = ["z", "d", "q", "m"];
+  fight = fight.split("");
+  for (let i = 0; i < fight.length; i++) {
+    if (right.includes(fight[i])) {
+      scoreRight += right.indexOf(fight[i]) + 1;
+      console.log(scoreRight);
+    } else {
+      scoreLeft += left.indexOf(fight[i]) + 1;
+      console.log(scoreLeft);
+    }
+  }
+  if (scoreRight > scoreLeft) {
+    return "Right side wins!";
+  } else if (scoreRight < scoreLeft) {
+    return "Left side wins!";
+  } else {
+    return "Let's fight again!";
+  }
+}
