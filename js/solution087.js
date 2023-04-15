129d6f65eab19e5fdf5131a3c7a58038ea6c9c40
marcadrian-it <marcadrian.it@gmail.com>
2023-04-15 18:58:01 +0200

    solution087.js

---
+++ js/solution087.js
@@ -0,0 +1,15 @@
+// // Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// // The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+// // Test.assertEquals(mygcd(30,12),6)
+// // Test.assertEquals(mygcd(8,9),1)
+// // Test.assertEquals(mygcd(1,1),1)
+// https://www.codewars.com/kata/5500d54c2ebe0a8e8a0003fd/
+
+function mygcd(x, y) {
+  return y == 0 ? x : mygcd(y, x % y);
+}
+console.log(mygcd(30, 12));
+console.log(mygcd(1, 1));
+console.log(mygcd(8, 9));
