b2f4383f7b62c67abaa70214e2fe8073bfd96275
marcadrian-it <marcadrian.it@gmail.com>
2023-07-19 12:52:00 +0200

    solution112.js

---
+++ js/solution112.js
@@ -0,0 +1,10 @@
+function adjacentElementsProduct(array) {
+  let maximum = -Infinity;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i + 1] !== undefined) {
+      const adjecentProduct = array[i] * array[i + 1];
+      adjecentProduct > maximum ? (maximum = adjecentProduct) : null;
+    }
+  }
+  return maximum;
+}
