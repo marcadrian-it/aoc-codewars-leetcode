30376e453ad6d5ff130431f268892402bdfcbb8a
marcadrian-it <marcadrian.it@gmail.com>
2023-07-19 12:59:21 +0200

    solution112.js

--- js/solution112.js
+++ js/solution112.js
@@ -1,6 +1,6 @@
 function adjacentElementsProduct(array) {
   let maximum = -Infinity;
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     if (array[i + 1] !== undefined) {
       const adjecentProduct = array[i] * array[i + 1];
       adjecentProduct > maximum ? (maximum = adjecentProduct) : null;
