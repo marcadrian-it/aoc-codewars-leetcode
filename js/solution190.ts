ebe71f90c8ab62ef273254be5c799b74e5225f41
marcadrian <marcadrian.it@gmail.com>
2023-11-24 19:00:54 +0100

    solution190.ts

---
+++ js/solution190.ts
@@ -0,0 +1,18 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmnt = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmnt;
+    for (; i < breaks.length; i += jumpAmnt) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jumpAmnt;
+
+    for (let j = 0; j < jumpAmnt && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
