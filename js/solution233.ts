e78d81e26435b86e6f52f4fc5cce9a2b052923f7
marcadrian-it <marcadrian.it@gmail.com>
2024-01-26 20:52:08 +0100

    feat(app): add solution233.ts
    A function icrementString that takes in a string argument and returns a new string with the number at the end incremented by 1

---
+++ js/solution233.ts
@@ -0,0 +1,19 @@
+function incrementString(str: string): string {
+  let numStr = "";
+  let index = str.length - 1;
+
+  while (index >= 0 && !isNaN(Number(str[index]))) {
+    numStr = str[index] + numStr;
+    index--;
+  }
+  if (numStr === "") {
+    return str + "1";
+  } else {
+    let num = parseInt(numStr) + 1;
+    let prefix = str.slice(0, index + 1);
+    let paddedNum = num.toString().padStart(numStr.length, "0");
+    return String(prefix + paddedNum);
+  }
+}
+
+console.log(incrementString("file002"));
