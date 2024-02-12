80dd6e3354b765b7ded322d4b7e97d569572878f
marcadrian-it <marcadrian.it@gmail.com>
2024-02-13 00:05:16 +0100

    feat(app): Queue implementation

---
+++ js/solution237.ts
@@ -0,0 +1,45 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
+export default class Queue<T> {
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
+
+  constructor() {
+    this.head = undefined;
+    this.tail = undefined;
+    this.length = 0;
+  }
+
+  enqueue(item: T): void {
+    const node = { value: item } as Node<T>;
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      return;
+    }
+    this.tail!.next = node;
+    this.tail = node;
+  }
+
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = undefined;
+    }
+    this.length--;
+    return value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+}
