253cbfaceeecb641eb4349f8aba13655d2385a64
marcadrian-it <marcadrian.it@gmail.com>
2024-02-15 00:36:43 +0100

    feat(app): Quick sort implementation
    Quick sort implementation in TS

---
+++ js/solution240.ts
@@ -0,0 +1,33 @@
+function qs(arr: number[], lo: number, hi: number): number[] | undefined {
+  if (lo >= hi) {
+    return;
+  }
+  const pivotIdx = partition(arr, lo, hi);
+
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx + 1, hi);
+  return arr;
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+  const pivot = arr[hi];
+
+  let idx = lo - 1;
+  for (let i = lo; i < hi; i++) {
+    if (arr[i] <= pivot) {
+      idx++;
+      const tmp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = tmp;
+    }
+  }
+  idx++;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+
+  return idx;
+}
+
+export default function quick_sort(arr: number[]): number[] | undefined {
+  return qs(arr, 0, arr.length - 1);
+}
